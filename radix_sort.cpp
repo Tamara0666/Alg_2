3543d66a55af1713e416addcf4551ac7039615b1
Tamara0666 <tamara.beber@student.um.si>
2026-03-18 13:38:11 +0100

    Finished input function

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -8,4 +8,15 @@ std::vector<unsigned char> readInput(const std::string& filename){
         std::cerr << "Error: can not open the file: "<< filename << "\n";
         return {};
     }
+    std::vector<unsigned char> numbers;
+    int x;
+    while(in >> x) {
+        if (x < 0 || x > 255)
+        {
+            std::cerr << "Error: number" << x << "is out of range (0, 255). \n";
+            continue;
+        }
+        numbers.push_back((unsigned char)x);
+    }
+    return numbers;
 }
