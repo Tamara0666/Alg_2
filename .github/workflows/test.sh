72f727f661e4b8fd7e59c043b27c7ef08aaad65d
Tamara <tamara.beber@student.um.si>
2026-04-27 15:03:15 +0200

    Update test.sh
    fixed

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -6,7 +6,7 @@ rm -f out.txt napaka.txt
 
 g++ -o program radix_sort.cpp || { echo "COMPILATION ERROR" > napaka.txt; exit 1; }
 
-./program vhod.txt || { echo "RUNTIME ERROR" > napaka.txt; exit 1; }
+./program ./vhod.txt || { echo "RUNTIME ERROR" > napaka.txt; exit 1; }
 
 # PRAVILNA KONTROLA SORTA
 sorted=$(tr ' ' '\n' < out.txt | sort -n | tr '\n' ' ')
