6ff190afd02eeabb0b61aa65fec2290cc2995e8b
Tamara <tamara.beber@student.um.si>
2026-04-27 10:32:02 +0200

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+rm -f out.txt input.txt
+
 g++ -o program radix_sort.cpp || exit 1
 
-# naredi input file
 echo "5 3 8 1 2" > input.txt
 
-# zaženi program
 ./program input.txt || exit 1
 
-# preberi output
 output=$(cat out.txt | tr -d '\n')
 
 expected="1 2 3 5 8"
